5e12db4d36d25a984a59cb6a7590d42a53e6098f
Chaiyawan Auepanwiriyakul <ca1216@ic.ac.uk>
2023-06-04 03:18:54 +0200

    Finish Server and Client

---
+++ RemoteDataset/Client.py
@@ -0,0 +1,68 @@
+import torch
+import pynng
+import msgpack
+import sshtunnel
+import msgpack_numpy
+
+
+msgpack_numpy.patch()
+
+
+class RemoteDataAdapter(torch.utils.data.Dataset):
+    def __init__(self, request_server, dataset_length, stat) -> None:
+        self.stat = stat
+        self.request_server = request_server
+        self.dataset_length = dataset_length
+        
+    def __len__(self):
+        return self.dataset_length
+    
+    def __getitem__(self, idx):
+        request = f"r:index:{idx}"
+        self.request_server.send(request.encode())
+        packed_data = self.request_server.recv()
+        data = msgpack.unpackb(packed_data)
+        
+        data = torch.from_numpy(data)
+        data = self.to_norm(data, self.stat[0], self.stat[1]).t()
+        
+        return data
+        
+    @staticmethod
+    def to_norm(data, means, stds):
+        norm = (data - means) / stds
+        return norm.float()
+
+
+
+class RemoteDataset:
+    def __init__(self, ssh_config, port, stat) -> None:
+        self.stat = stat
+        self.port = port
+        self.tun = sshtunnel.SSHTunnelForwarder(
+            "apollo.doc.ic.ac.uk",
+            ssh_username=ssh_config["SSH_USERNAME"],
+            ssh_password=ssh_config["SSH_PASSWORD"],
+            ssh_port=10022,
+            remote_bind_address=('127.0.0.1', port),
+            local_bind_address=("", port)
+        )
+        self.request_server = pynng.Req0()
+        
+    def connect(self):
+        self.tun.start()
+        self.request_server.dial(f"tcp://127.0.0.1:{self.port}")
+        
+    def __len__(self):
+        self.request_server.send(b"r:start")
+        datasets = self.request_server.recv()
+        self.datasets = msgpack.unpackb(datasets)
+        return len(self.datasets)
+    
+    def __iter__(self):
+        for subj, dates in self.datasets.items():
+            for date in dates:
+                request = "_".join([subj, date])
+                self.request_server.send(b"r:dataset:" + request.encode())
+                dataset_length = int(self.request_server.recv())
+                yield subj, date.split("_")[0], RemoteDataAdapter(self.request_server, dataset_length, self.stat)
--- RemoteDataset/Server.py
+++ RemoteDataset/Server.py
@@ -1,11 +1,12 @@
 import os
-import socket
+import pynng
 import msgpack
 import argparse
 import numpy as np
 import msgpack_numpy
 
 from glob import glob
+from time import sleep
 
 
 msgpack_numpy.patch()
@@ -27,6 +28,88 @@ parser.add_argument(
     help='Port'
 )
 
+parser.add_argument(
+    '--debug',
+    type=int,
+    help='Debug'
+)
+
 
 def main(args):
+    print("Looking for files...")
+    file_list = glob(os.path.join(args.main_path, args.glob_pattern))
     
+    dataset = {}
+    for file in file_list:
+        subj, *date = file.split(os.sep)[-1].split("_")
+        date = "_".join(date)
+        subj_dates_list = dataset.get(subj, [])
+        if date in subj_dates_list:
+            raise ValueError("Duplicate entries found")
+
+        subj_dates_list.append(date)
+        dataset[subj] = subj_dates_list
+        
+    print("Starting server...")
+    reply_server = pynng.Rep0()
+    reply_server.listen(f"tcp://127.0.0.1:{args.port}")
+    
+    request = reply_server.recv().decode().split(":")
+    if request[0] != "r":
+        raise ValueError("Invalid request")
+    
+    reply = msgpack.packb(dataset)
+    reply_server.send(reply)
+    
+    while True:
+        request = reply_server.recv().decode().split(":")
+        if request[0] != "r":
+            raise ValueError("Invalid request")
+        
+        if request[1] == "dataset":
+            if args.debug:
+                print(f"Sending dataset {request[2]}")
+
+            data = np.load(os.path.join(args.main_path, request[2]))["resampled_matrices"]
+            data = np.hstack(
+                [
+                    data[0, :, 4:7],
+                    data[0, :, 1:4],
+                    # data[:, 3:6],
+
+                    data[1, :, 4:7],
+                    data[1, :, 1:4],
+                    # data[:, 12:15],
+
+                    data[2, :, 4:7],
+                    data[2, :, 1:4],
+                    # data[:, 21:24],
+
+                    data[3, :, 4:7],
+                    data[3, :, 1:4],
+                    # data[:, 30:33],
+                ]
+            )
+            
+            data_slice_start = np.arange(0, data.shape[0] - 80 + 1, 80).astype(np.int64)
+            reply = f"{len(data_slice_start)}".encode()
+            reply_server.send(reply)
+        
+        if request[1] == "index":
+            if args.debug:
+                print(f"Sending index {int(request[2])}")
+
+            starting_index = int(request[2])
+            data_chunk = data[starting_index:starting_index + 80]
+            reply = msgpack.packb(data_chunk)
+            reply_server.send(reply)
+        
+        if request[1] == "close":
+            print("Closing server...")
+            sleep(1)
+            reply_server.close()
+            break
+        
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args)
